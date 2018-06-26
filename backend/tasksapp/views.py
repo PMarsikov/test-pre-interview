a66a65078d50b4e0b5db4e0c4f348a56798da9f5
Pavel Marsikau <PMarsikau@gomel.iba.by>
2018-06-26 17:54:33 +0300

    removed comments

--- backend/tasksapp/views.py
+++ backend/tasksapp/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 
 from api.models import Task
 
-# Create your views here.
 def index(request):
-    # tasks = Task.objects.order_by('-date_created')
-    # context = {'tasks': tasks}
-
     return render(request, 'tasksapp/index.html')
